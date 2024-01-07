bef18b54c99f416859438de613ec22e681ece146
Mikyx1 <92131994+Mikyx-1@users.noreply.github.com>
2024-01-07 12:45:22 +0700

    Update clean_data.py

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,3 +1,4 @@
+                                                                                             
 from bisenet import BiSeNet
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,10 +18,10 @@ model = BiSeNet(2)
 model.eval()
 model.to(device)
 model.activate_evaluation_mode()
-model.load_state_dict(torch.load("./bisenet_360_trial_7/last.pt", map_location="cpu"))
+model.load_state_dict(torch.load("./bisenet_360_trial_7/val_loss_min.pt", map_location="cpu"))
 
-img_dirs = sorted(glob.glob("/home/edmond/Desktop/instance-level-human-parsing/images/*"))
-mask_dirs = sorted(glob.glob("/home/edmond/Desktop/instance-level-human-parsing/masks/*"))
+img_dirs = sorted(glob.glob("../train_dataset/images/*"))
+mask_dirs = sorted(glob.glob("../train_dataset/masks/*"))
 print(f"Num images: {len(img_dirs)}, Num masks: {len(mask_dirs)}")
 
 
@@ -28,14 +29,18 @@ transform = A.Compose([A.Resize(360, 360),
                       ToTensorV2()])
 
 ious = []
-for ith, img_dir in enumerate(img_dirs[1:]):
-    image = cv2.imread(img_dir).astype("float32")/255.
-    mask = cv2.imread(mask_dirs[ith+1], cv2.IMREAD_GRAYSCALE)
+for i in tqdm(range(len(img_dirs))):
+    image = cv2.imread(img_dirs[i]).astype("float32")/255.
+    mask = cv2.imread(mask_dirs[i], cv2.IMREAD_GRAYSCALE)
     mask = np.where(mask==2, 1, 0)
     mask = cv2.resize(mask.astype("uint8"), (360, 360))
     transformed = transform(image=image)["image"][None, ...]
     pred = model(transformed).argmax(1)[0]
-    iou = 2*(pred*mask).sum()/(pred.sum() + mask.sum())
+    iou = 2*(pred*mask + 0.0001).sum()/(pred.sum() + mask.sum() + 0.0001)
     ious.append([img_dir, iou])
 
 sorted_ious = sorted(ious, key=lambda x: x[1])
+with open("sorted_ious.txt", "w") as file:
+  for sorted_iou in tqdm(sorted_ious):
+    file.write(sorted_iou[0] + " " + str(sorted_iou[1]) + "\n")
+
