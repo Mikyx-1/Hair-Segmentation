ade469140c826a00c4e567b39d94439c894d4ca2
Mikyx1 <92131994+Mikyx-1@users.noreply.github.com>
2024-01-16 10:48:59 +0700

    Update inference_on_video.py

--- robust_video_matting/inference_on_video.py
+++ robust_video_matting/inference_on_video.py
@@ -1,6 +1,4 @@
-import av
 import os
-import pims
 import numpy as np
 from torch.utils.data import Dataset
 from torchvision.transforms.functional import to_pil_image
@@ -9,29 +7,12 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 import torch
 import time
+import cv2
+from albumentations.pytorch import ToTensorV2
+import albumentations as A
 
 torch.set_grad_enabled(False)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-class VideoReader(Dataset):
-    def __init__(self, path, transform=None):
-        self.video = pims.PyAVVideoReader(path)
-        self.rate = self.video.frame_rate
-        self.transform = transform
-        
-    @property
-    def frame_rate(self):
-        return self.rate
-    
-    def __len__(self):
-        return len(self.video)
-    
-    def __getitem__(self, idx):
-        frame = self.video[idx]
-        frame = Image.fromarray(np.asarray(frame))
-        if self.transform is not None:
-            frame = self.transform(frame)
-        return frame
 
 def auto_downsample_ratio(h, w):
     return min(512/max(h, w), 1)
@@ -41,18 +22,28 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-source = VideoReader("./3_0.mp4", transform)
 model = torch.hub.load("PeterL1n/RobustVideoMatting", "mobilenetv3")
 model = model.eval()
 model = model.to(device)
-param = next(model.parameters())
-dtype = param.dtype
 
+cap = cv2.VideoCapture("./3_0.mp4")
 downsample_ratio = 1
 rec = [None]*4
 
-for i in range(len(source)):
-    src = source[i]
-    fgr, pha, *rec = model(src.unsqueeze(0), *rec, downsample_ratio)
+transform = A.Compose([
+    A.Resize(360, 360),
+    ToTensorV2()
+])
+
+bgr = torch.tensor([120, 255, 155], device=device, dtype=torch.float32).div(255).view(1, 1, 3, 1, 1)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        transformed = frame.copy()[:, :, ::-1].astype("float32")/255.
+        transformed = transform(image=transformed)["image"][None, ...].to(device)
+        fgr, pha, *rec = model(transformed, *rec, 1)
+        com = fgr*pha + bgr*(1-pha) # Shape: 1 x 1 x channels x height x width
+        break
     break
-    
+
